0f9c28ac8189c48b14b4aba0bbb65e969bed32ce
davalmeyda <dav.almeyda@gmail.com>
2022-07-10 12:31:11 -0500

    comentarios en api

--- src/config/backend.js
+++ src/config/backend.js
@@ -11,11 +11,11 @@ export const urlImagen = 'http://192.168.4.230/historial-clinico-backend/public/
 // export const baseUrlDavid = 'http://23.254.228.60/historial-clinico-backend/public/api';
 // export const urlImagen = 'http://23.254.228.60/historial-clinico-backend/public/';
 
-export const baseUrl = 'http://127.0.0.1:8000/api';
-export const baseUrlImage = 'http://127.0.0.1:8000';
+export const baseUrl = 'http://127.0.0.1:8000/api'; // MODIFICAR EN PRODUCCION
+export const baseUrlImage = 'http://127.0.0.1:8000'; // MODIFICAR EN PRODUCCION
 export const baseUrlNew = 'http://192.236.179.158/backend/public/api';
 
-export const baseUrlReports = 'http://127.0.0.1:8000/api';
+export const baseUrlReports = 'http://127.0.0.1:8000/api'; // MODIFICAR EN PRODUCCION
 export const baseUrlReportsNew = 'http://192.236.179.158/reportes/public/api';
 
 //CONEXION BACKEND PRODUCCION
