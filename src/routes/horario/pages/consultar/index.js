25aab3a76a5c0f6b476ede5593b5fba14a1d34b7
Bruno Breña Ramírez <brcalix@gmail.com>
2022-07-14 01:01:26 -0500

    feat: seleccionar mes

--- src/routes/horario/pages/consultar/index.js
+++ src/routes/horario/pages/consultar/index.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Button } from 'antd';
+import { Card, Button, DatePicker, Spin } from 'antd';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import { httpClient } from '../../../../util/Api';
 import ModalAsignar from './ModalAsignar';
 import ModalEditar from './ModalEditar';
 
-import ReactExport  from "react-export-excel";
+import ReactExport from 'react-export-excel';
 
 const ExcelFile = ReactExport.ExcelFile;
 const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
@@ -36,16 +36,19 @@ const messages = {
 
 const ConsultarHorario = () => {
 	const [events, setEvents] = useState([]);
-  const [dataFormat, setDataFormat] = useState({});
+	const [dataFormat, setDataFormat] = useState({});
 	const [visibleModalAsignar, setVisibleModalAsignar] = useState(false);
+	const [loading, setLoading] = useState(false);
 	const [visibleModalEditar, setVisibleModalEditar] = useState(false);
 	const [currentMes, setCurrentMes] = useState(moment().month() + 1);
+	const [fechaSeleccionada, setFechaSeleccionada] = useState(new Date());
+	const [datePicker, setDatePicker] = useState(new Date());
 	const [medico, setMedico] = useState({});
-
+	console.log(fechaSeleccionada);
 	const agregarEvento = data => {
 		var aux = [];
 		data.forEach(element => {
-      // console.log(element);
+			// console.log(element);
 			var start = new Date(
 				moment(element.fecha).format('YYYY'),
 				Number(moment(element.fecha).format('MM')) - 1,
@@ -81,20 +84,22 @@ const ConsultarHorario = () => {
 				start,
 				end,
 				id: element.id_horario,
-        turno: () => {
-          const horaInicio = Number(moment(element.hora_inicio, 'HH:mm').format('H'));
-          const horaFin = Number(moment(element.hora_fin, 'HH:mm').format('H'));
-          let horaInicioLabel = '';
-          let horaFinLabel = '';
+				turno: () => {
+					const horaInicio = Number(moment(element.hora_inicio, 'HH:mm').format('H'));
+					const horaFin = Number(moment(element.hora_fin, 'HH:mm').format('H'));
+					let horaInicioLabel = '';
+					let horaFinLabel = '';
 
-          if (horaInicio >= 0 && horaInicio < 8) horaInicioLabel = 'Mañana';
-          else if (horaInicio >= 8 && horaInicio < 16) horaInicioLabel = 'Tarde';
-          else if (horaInicio >= 16 && horaInicio < 24) horaInicioLabel = 'Noche';
-          if (horaFin >= 0 && horaFin < 8) horaFinLabel = 'Mañana';
-          else if (horaFin >= 8 && horaFin < 16) horaFinLabel = 'Tarde';
-          else if (horaFin >= 16 && horaFin < 24) horaFinLabel = 'Noche';
-          return horaInicioLabel === horaFinLabel ? horaInicioLabel : `${horaInicioLabel} - ${horaFinLabel}`;
-        }
+					if (horaInicio >= 0 && horaInicio < 8) horaInicioLabel = 'Mañana';
+					else if (horaInicio >= 8 && horaInicio < 16) horaInicioLabel = 'Tarde';
+					else if (horaInicio >= 16 && horaInicio < 24) horaInicioLabel = 'Noche';
+					if (horaFin >= 0 && horaFin < 8) horaFinLabel = 'Mañana';
+					else if (horaFin >= 8 && horaFin < 16) horaFinLabel = 'Tarde';
+					else if (horaFin >= 16 && horaFin < 24) horaFinLabel = 'Noche';
+					return horaInicioLabel === horaFinLabel
+						? horaInicioLabel
+						: `${horaInicioLabel} - ${horaFinLabel}`;
+				},
 			});
 		});
 		setEvents([...aux]);
@@ -108,12 +113,14 @@ const ConsultarHorario = () => {
 	};
 
 	const traerData = async () => {
+		setLoading(true);
 		const response = await httpClient.post('/horarios/getHorarioFecha', {
 			mes: currentMes,
 		});
 
 		// console.log(response.data.data);
 		agregarEvento(response.data.data);
+		setLoading(false);
 	};
 
 	useEffect(() => {
@@ -121,21 +128,28 @@ const ConsultarHorario = () => {
 	}, [currentMes]);
 
 	useEffect(() => {
+		if (datePicker) {
+			setCurrentMes(moment(datePicker).month() + 1);
+			setFechaSeleccionada(datePicker);
+		}
+	}, [datePicker]);
+
+	useEffect(() => {
 		traerData();
 	}, []);
 
-  useEffect(() => {
-    const dataNew = {};
-    events.forEach(item => {
-      if (Object.keys(dataNew).find(x => x === item.especialidad)) {
-        dataNew[item.especialidad] = [...dataNew[item.especialidad], item];
-      } else {
-        dataNew[item.especialidad] = [item];
-      }
-    });
-    console.log(dataNew);
-    setDataFormat(dataNew);
-  }, [events])
+	useEffect(() => {
+		const dataNew = {};
+		events.forEach(item => {
+			if (Object.keys(dataNew).find(x => x === item.especialidad)) {
+				dataNew[item.especialidad] = [...dataNew[item.especialidad], item];
+			} else {
+				dataNew[item.especialidad] = [item];
+			}
+		});
+		console.log(dataNew);
+		setDataFormat(dataNew);
+	}, [events]);
 
 	return (
 		<Card
@@ -165,38 +179,51 @@ const ConsultarHorario = () => {
 					>
 						{/*<Button*/}
 
-            {/*  onClick={handleExportExcel}*/}
+						{/*  onClick={handleExportExcel}*/}
 						{/*>*/}
 						{/*	Exportar*/}
 						{/*</Button>*/}
-            <ExcelFile element={<Button style={{
-              backgroundColor: 'blue',
-              color: 'white',
-              marginTop: '10px',
-            }}>Exportar</Button>}>
-              {Object.keys(dataFormat).map(item => (
-                <ExcelSheet
-                  data={dataFormat[item]}
-                  name={item}
-                >
-                  <ExcelColumn label="ESPECIALIDAD" value="especialidad"/>
-                  <ExcelColumn label="DOCTOR A CARGO" value="nombre_medico"/>
-                  <ExcelColumn label="HORARIO DE ATENCIÓN" value={(col) => `${moment(col.hora_inicio, 'HH:mm').format('hh a')} a ${moment(col.hora_fin, 'HH:mm').format('hh a')}`} />
-                  <ExcelColumn
-                    label="TURNO"
-                    value={(col) => `${col.turno()}`}
-                    style={{
-                      alignment: {
-                        vertical: 'center',
-                        horizontal: 'center'
-                      },
-                      font: { sz: "12", bold: true }
-                    }}
-                  />
-                  {/*<ExcelColumn label="REQUISITOS PARA CONSULTAS" value={(col) => ``}/>*/}
-                </ExcelSheet>
-              ))}
-            </ExcelFile>
+						<ExcelFile
+							element={
+								<Button
+									style={{
+										backgroundColor: 'blue',
+										color: 'white',
+										marginTop: '10px',
+									}}
+								>
+									Exportar
+								</Button>
+							}
+						>
+							{Object.keys(dataFormat).map(item => (
+								<ExcelSheet data={dataFormat[item]} name={item}>
+									<ExcelColumn label="ESPECIALIDAD" value="especialidad" />
+									<ExcelColumn label="DOCTOR A CARGO" value="nombre_medico" />
+									<ExcelColumn
+										label="HORARIO DE ATENCIÓN"
+										value={col =>
+											`${moment(col.hora_inicio, 'HH:mm').format('hh a')} a ${moment(
+												col.hora_fin,
+												'HH:mm'
+											).format('hh a')}`
+										}
+									/>
+									<ExcelColumn
+										label="TURNO"
+										value={col => `${col.turno()}`}
+										style={{
+											alignment: {
+												vertical: 'center',
+												horizontal: 'center',
+											},
+											font: { sz: '12', bold: true },
+										}}
+									/>
+									{/*<ExcelColumn label="REQUISITOS PARA CONSULTAS" value={(col) => ``}/>*/}
+								</ExcelSheet>
+							))}
+						</ExcelFile>
 						<Button
 							style={{
 								backgroundColor: '#04B0AD',
@@ -214,21 +241,51 @@ const ConsultarHorario = () => {
 			}
 		>
 			<div className="gx-main-content">
-				<div className="gx-rbc-calendar">
-					<Calendar
-						popup
-						events={[...events]}
-						toolbar={true}
-						step={60}
-						messages={messages}
-						localizer={localizer}
-						onNavigate={date => {
-							setCurrentMes(date.getMonth() + 1);
+				<div
+					style={{
+						display: 'flex',
+						flexDirection: 'row-reverse',
+						width: '100%',
+						marginBottom: '20px',
+					}}
+				>
+					<DatePicker
+						picker={'month'}
+						style={{ width: '200px' }}
+						value={moment(datePicker)}
+						onChange={e => {
+							setDatePicker(new Date(e.format('YYYY-MM-DD')));
 						}}
-						views={{ month: true }}
-						onSelectEvent={event => mostrarModalDetalle(event)}
-						defaultDate={new Date()}
 					/>
+				</div>
+				<div className="gx-rbc-calendar">
+					{loading ? (
+						<div
+							style={{
+								display: 'flex',
+								justifyContent: 'center',
+								flexDirection: 'column',
+								height: '500px',
+							}}
+						>
+							<Spin tip="Cargando" />
+						</div>
+					) : (
+						<Calendar
+							popup
+							events={[...events]}
+							toolbar={true}
+							step={60}
+							messages={messages}
+							localizer={localizer}
+							onNavigate={date => {
+								setCurrentMes(date.getMonth() + 1);
+							}}
+							views={{ month: true }}
+							onSelectEvent={event => mostrarModalDetalle(event)}
+							date={fechaSeleccionada}
+						/>
+					)}
 				</div>
 				{visibleModalAsignar ? (
 					<ModalAsignar
